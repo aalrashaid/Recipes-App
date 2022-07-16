b2e2a0ccf23396a14856e1d3eeb3992fc434da37
aalrashaid <a.alrashid@me.com>
2022-07-16 16:21:03 +0800

    Route of Controller All

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,20 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::GET('Profile/index',[ProfilesController::class, 'index'])->name('Profiles.index');
+Route::GET('Profile/create',[ProfilesController::class, 'create'])->name('Profiles.create');
+Route::POST('Profile/store',[ProfilesController::class, 'store'])->name('Profiles.store');
+Route::GET('Profile/show/{id}',[ProfilesController::class, 'show'])->name('Profiles.show');
+Route::GET('Profile/{id}/edit',[ProfilesController::class, 'edit'])->name('Profiles.edit');
+Route::PATCH('Profile/update',[ProfilesController::class, 'update'])->name('Profiles.update');
+Route::DELETE('Profile/{id}/Destroy',[ProfilesController::class, 'destroy'])->name('Profiles.destroy');
+
+Route::GET('Recipes/index', [RecipesController::class ,'index'])->name('Recipes.index');
+Route::GET('Recipes/create', [RecipesController::class ,'create'])->name('Recipes.create');
+Route::POST('Recipes/store', [RecipesController::class ,'store'])->name('Recipes.store');
+Route::GET('Recipes/show/{id}', [RecipesController::class ,'show'])->name('Recipes.show');
+Route::GET('Recipes/{id}/edit', [RecipesController::class ,'edit'])->name('Recipes.edit');
+Route::PATCH('Recipes/update', [RecipesController::class ,'update'])->name('Recipes.update');
+Route::DELETE('Recipes/{id}/Destroy', [RecipesController::class ,'destroy'])->name('Recipes.destroy');
+
